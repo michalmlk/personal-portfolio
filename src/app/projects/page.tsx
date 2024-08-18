552b544441219574e4130b187bdcf039ec0fa6c7
michalmlk <michal.melka1@gmail.com>
2024-08-18 12:00:42 +0200

    make other sections adjustable

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,29 +1,16 @@
-import { performRequest } from '@/app/api/datocms';
+import { generateSubscription, performRequest } from '@/app/api/datocms';
 import { PROJECTS_QUERY } from '@/app/api/queries';
 import styles from './styles.module.scss';
-import ProjectsList from '@/app/projects/components/ProjectsList/ProjectsList';
-import { Suspense } from 'react';
 import RealtimeProjectsList from '@/app/projects/components/ProjectsList/ProjectsList';
 
 export default async function Projects() {
-    const initialData = await performRequest(PROJECTS_QUERY);
-
-    console.log(initialData);
+    const subscription = await generateSubscription(PROJECTS_QUERY);
 
     return (
         <div className='page-wrapper'>
             <div className={styles.wrapper} id='projects'>
                 <h1 className='section-header'>Projects</h1>
-                <Suspense fallback={'loading...'}>
-                    <RealtimeProjectsList
-                        subscription={{
-                            query: PROJECTS_QUERY,
-                            initialData,
-                            environment: process.env.NEXT_DATOCMS_ENVIRONMENT,
-                            token: process.env.NEXT_DATOCMS_API_TOKEN,
-                        }}
-                    />
-                </Suspense>
+                <RealtimeProjectsList subscription={subscription} />
             </div>
         </div>
     );
